2adfce11d56d2cc97df0389aa6e9c784af6554be
pedronunes19 <pedromiguelmnunes@gmail.com>
2021-08-07 04:19:04 +0100

    updated --comandos + commented out --amen (not functioning now with heroku)

--- bot.py
+++ bot.py
@@ -111,13 +111,14 @@ async def pcp(ctx):
             await ctx.send(line)
             time.sleep(1)
     file.close()
-    
 
+# useless since the picture isn't on github    
+"""  
 @bot.command(name = 'amen')
 async def amen(ctx):
     await ctx.send('Um momento para a palavra do senhor')
     await ctx.send(file=discord.File('amen.jpg'))
-
+"""
 
 
 @bot.command(name = 'sporting')
@@ -166,9 +167,10 @@ async def comandos(ctx):
     command_embed = discord.Embed(title = 'Lista de comandos', color = 0xe44a5d )
     command_embed.add_field(name = '--teste', value = 'Teste', inline = False)
     command_embed.add_field(name = '--tino', value = 'Escreve a maravilhosa letra da música "Pão com Manteiga"', inline = False)
+    command_embed.add_field(name = '--leopoldina', value = 'são 4 da manhã não sei o que escrever', inline = False)
     command_embed.add_field(name = '--pcp', value = 'AVANTE CAMARADA', inline = False)
     command_embed.add_field(name = '--portugal', value = 'Hino de Portugal', inline = False)
-    command_embed.add_field(name = '--amen', value = 'Pequeno momento para a palavra do senhor', inline = False)
+    #command_embed.add_field(name = '--amen', value = 'Pequeno momento para a palavra do senhor', inline = False)
     command_embed.add_field(name = '--sporting', value = 'Se o teu amor é o Sporting', inline = False)
     command_embed.add_field(name = '--translate', value = '"--translate <lang> <text>" para traduzir texto', inline = False)
     command_embed.add_field(name = '--repo', value = 'Link para o repositório do github', inline = False)
